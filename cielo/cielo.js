b7700b46cec743fc497afb9a558a4039c0abc86a
eunidayo <wkdal3358@gmail.com>
2024-06-03 12:44:56 +0900

    06/03 오전 업로드

--- cielo/cielo.js
+++ cielo/cielo.js
@@ -1,3 +1,4 @@
+// sec01
 var swiper = new Swiper(".mySwiper", {
   zoom: true,
   navigation: {
@@ -8,6 +9,23 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
 });
 
+// sec02
+var swiper = new Swiper(".sec02mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 100,
+  freeMode: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false
+  }
+});
